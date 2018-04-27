21de6608da9bbec8b6375ef5138691e8e70de062
Simon <git@sbstp.ca>
2018-04-27 14:48:45 -0400

    update

--- extract-nominal.py
+++ extract-nominal.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import re
 
 RE_SEPARATOR = re.compile(r"[\n\t\s\[\]]+")
+N_WORDS = 4
+STOP_WORDS = set(word.strip() for word in Path('stoplist-english.txt').read_text().split("\n") if word.strip())
 
 
 class Word:
@@ -35,7 +37,9 @@ def printf(fmt, *args, **kwargs):
 def format_iterable(items):
     pieces = []
     for item in items:
-        if isinstance(item, str):
+        if item is None:
+            pieces.append('?')
+        elif isinstance(item, str):
             pieces.append("'" + escape(item) + "'")
         elif isinstance(item, int):
             pieces.append(str(item))
@@ -50,7 +54,26 @@ def get(list, index, default=None):
     try:
         return list[index]
     except IndexError:
-        return Word("", "")
+        return None
+
+
+def find_context(words, pos, prev=False, n=N_WORDS/2):
+    """
+    Find the n previous/next non stop words in the list of words.
+    If we run out of bounds in the list of words, None is used.
+    """
+    direction = -1 if prev else 1
+    selection = []
+    while n > 0:
+        pos += direction
+        sel = get(words, pos)
+        if sel is None:
+            n -= 1
+            selection.append(None)
+        elif sel.text not in STOP_WORDS:
+            selection.append(sel)
+            n -= 1
+    return selection
 
 
 raw_data = Path("interest.acl94.txt").read_text()
@@ -61,7 +84,6 @@ for sample in samples:
     words = [Word(*wordcat.split("/"))
              for wordcat in RE_SEPARATOR.split(sample)
              if "/" in wordcat]
-    print(len(words), len(RE_SEPARATOR.split(sample)))
     if not words:
         continue
     pos = None
@@ -71,19 +93,20 @@ for sample in samples:
             pos = idx
             meaning = int(word.text.split('_')[1])
             break
-    e = Entry(meaning, [get(words, pos - 2), get(words, pos - 1), get(words, pos + 1), get(words, pos + 2)])
-    entries.append(e)
 
-exit()
-N_WORDS = 4
+    # e = Entry(meaning, [get(words, pos - 2), get(words, pos - 1), get(words, pos + 1), get(words, pos + 2)])
+    e = Entry(meaning, find_context(words, pos, prev=True) + find_context(words, pos))
+    assert len(e.words) == N_WORDS
+    entries.append(e)
 
 nominal_word_sets = [set() for _ in range(N_WORDS)]
 nominal_cat_sets = [set() for _ in range(N_WORDS)]
 
 for entry in entries:
     for idx, word in enumerate(entry.words):
-        nominal_word_sets[idx].add(word.text)
-        nominal_cat_sets[idx].add(word.cat)
+        if word is not None:
+            nominal_word_sets[idx].add(word.text)
+            nominal_cat_sets[idx].add(word.cat)
 
 printf("@relation interest")
 printf("")
@@ -99,6 +122,10 @@ printf("@data")
 for entry in entries:
     row = [entry.meaning]
     for word in entry.words:
-        row.append(word.text)
-        row.append(word.cat)
+        if word is not None:
+            row.append(word.text)
+            row.append(word.cat)
+        else:
+            row.append(None)
+            row.append(None)
     print_row(*row)
